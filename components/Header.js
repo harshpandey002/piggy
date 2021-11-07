9449f2d1c56232a9ece4e67fa7659d5f278061c6
harshpandey002 <coding.harshp@gmail.com>
2021-11-07 20:51:24 +0530

    Header

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,17 @@ import styles from "@/styles/Header.module.css";
 export default function Header() {
   return (
     <div className={styles.container}>
-      <h2>Header</h2>
+      <nav>
+        <div className={styles.logo}>
+          <h1>Piggy</h1>
+        </div>
+
+        <div className={styles.navItems}>
+          <div className={styles.time}>
+            <p>01 Nov | 12:44AM</p>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }

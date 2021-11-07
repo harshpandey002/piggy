cdb6a04594f8fdf96858f794108b6137ce9c4e95
harshpandey002 <coding.harshp@gmail.com>
2021-11-07 22:06:34 +0530

    Responsive Links

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,30 @@
 import styles from "@/styles/Nav.module.css";
-
+import {
+  AiOutlineDollarCircle,
+  AiOutlineHome,
+  AiOutlineCreditCard,
+  AiOutlineSetting,
+} from "react-icons/ai";
 export default function Nav() {
   return (
     <div className={styles.container}>
-      <ul style={{ listStyle: "none" }}>
-        <li>Dashboard</li>
-        <li>Transaction</li>
-        <li>Budget</li>
-        <li>Settings</li>
+      <ul className={styles.links}>
+        <li>
+          <AiOutlineHome className={styles.icon} />
+          <span>Dashboard</span>
+        </li>
+        <li>
+          <AiOutlineCreditCard className={styles.icon} />
+          <span>Transaction</span>
+        </li>
+        <li>
+          <AiOutlineDollarCircle className={styles.icon} />
+          <span>Budget</span>
+        </li>
+        <li>
+          <AiOutlineSetting className={styles.icon} />
+          <span>Settings</span>
+        </li>
       </ul>
     </div>
   );

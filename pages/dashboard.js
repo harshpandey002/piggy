c85269e2f522a6edd7bd1a2d7e5018fdd3d479a1
harshpandey002 <coding.harshp@gmail.com>
2021-11-08 18:59:51 +0530

    Dashboard Inside Structure

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,7 +4,13 @@ import Layout from "@/components/Layout";
 export default function Dashboard() {
   return (
     <Layout>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.left}></div>
+        <div className={styles.right}>
+          <div className={styles.top}></div>
+          <div className={styles.bottom}></div>
+        </div>
+      </div>
     </Layout>
   );
 }

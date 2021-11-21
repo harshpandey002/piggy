c3f95f00f98da70ed7c55cf122438246de6b7a14
harshpandey002 <coding.harshp@gmail.com>
2021-11-21 21:41:49 +0530

    Line Chart

--- components/Graph.js
+++ components/Graph.js
@@ -1,9 +1,64 @@
 import styles from "@/styles/Graph.module.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Page A",
+    pv: 2400,
+  },
+  {
+    name: "Page B",
+    pv: 1398,
+  },
+  {
+    name: "Page C",
+    pv: 9800,
+  },
+  {
+    name: "Page D",
+    pv: 3908,
+  },
+  {
+    name: "Page D",
+    pv: 3908,
+  },
+  {
+    name: "Page E",
+    pv: 4800,
+  },
+  {
+    name: "Page F",
+    pv: 3800,
+  },
+  {
+    name: "Page G",
+    pv: 4300,
+  },
+];
 
 export default function Graph() {
   return (
     <div className={styles.container}>
-      <p>Something</p>
+      <div className={` box ${styles.content}`}>
+        <ResponsiveContainer>
+          <LineChart data={data}>
+            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Line type="monotone" dataKey="pv" stroke="#000" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }

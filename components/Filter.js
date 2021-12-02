8e4b8ba7ed6822269a92c926e4d9497c0a30a659
harshpandey002 <coding.harshp@gmail.com>
2021-12-02 16:30:53 +0530

    Filter component

---
+++ components/Filter.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import styles from "@/styles/Filter.module.css";
+import Select from "react-select";
+import ReactSlider from "react-slider";
+import { dropStyles, getTheme, categoryOp } from "@/util/common";
+import { DateRange } from "react-date-range";
+
+export default function Filter() {
+  const [keyword, setKeyword] = useState("");
+  const [category, setCategory] = useState([]);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
+  return (
+    <div className={` box ${styles.container}`}>
+      <div className={styles.input}>
+        <p>By category</p>
+        <Select
+          styles={dropStyles}
+          options={categoryOp}
+          theme={getTheme}
+          value={category}
+          isClearable
+          onChange={setCategory}
+          isSearchable={false}
+          placeholder="Select Category"
+        />
+      </div>
+      <div className={styles.input}>
+        <p>By note</p>
+        <input
+          type="search"
+          value={keyword}
+          placeholder="By keyword"
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </div>
+      <div className={styles.input}>
+        <p>By amount</p>
+        <ReactSlider
+          className={styles.sliderContainer}
+          thumbClassName={styles.sliderThumb}
+          onChange={(value, index) =>
+            console.log(`onChange: ${JSON.stringify({ value, index })}`)
+          }
+          trackClassName="sliderTrack"
+          defaultValue={[0, 100]}
+          ariaLabel={["Lower thumb", "Upper thumb"]}
+          ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
+          pearling
+          minDistance={10}
+        />
+      </div>
+      {/* <DateRange
+              editableDateInputs={true}
+              onChange={(item) => setDate([item.selection])}
+              moveRangeOnFirstSelection={false}
+              ranges={date}
+            /> */}
+    </div>
+  );
+}
--- pages/transaction/index.js
+++ pages/transaction/index.js
@@ -2,24 +2,13 @@ import { useState } from "react";
 import Layout from "@/components/Layout";
 import styles from "@/styles/Transaction.module.css";
 import { transactions } from "@/util/content";
-import Select from "react-select";
-import ReactSlider from "react-slider";
-import { dropStyles, getTheme, categoryOp } from "@/util/common";
-import { DateRange } from "react-date-range";
+
 import ModalTransaction from "@/components/ModalTransaction";
+import Filter from "@/components/Filter";
 
 export default function Transaction() {
-  const [keyword, setKeyword] = useState("");
-  const [category, setCategory] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [editMode, setEditMode] = useState(false);
-  const [date, setDate] = useState([
-    {
-      startDate: new Date(),
-      endDate: null,
-      key: "selection",
-    },
-  ]);
 
   const handleAdd = () => {
     setEditMode(true);
@@ -48,52 +37,7 @@ export default function Transaction() {
               />
             ))}
           </div>
-          <div className={` box ${styles.right}`}>
-            <div className={styles.input}>
-              <p>By category</p>
-              <Select
-                styles={dropStyles}
-                options={categoryOp}
-                theme={getTheme}
-                value={category}
-                isClearable
-                onChange={setCategory}
-                isSearchable={false}
-                placeholder="Select Category"
-              />
-            </div>
-            <div className={styles.input}>
-              <p>By note</p>
-              <input
-                type="search"
-                value={keyword}
-                placeholder="By keyword"
-                onChange={(e) => setKeyword(e.target.value)}
-              />
-            </div>
-            <div className={styles.input}>
-              <p>By amount</p>
-              <ReactSlider
-                className={styles.sliderContainer}
-                thumbClassName={styles.sliderThumb}
-                onChange={(value, index) =>
-                  console.log(`onChange: ${JSON.stringify({ value, index })}`)
-                }
-                trackClassName="sliderTrack"
-                defaultValue={[0, 100]}
-                ariaLabel={["Lower thumb", "Upper thumb"]}
-                ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
-                pearling
-                minDistance={10}
-              />
-            </div>
-            {/* <DateRange
-              editableDateInputs={true}
-              onChange={(item) => setDate([item.selection])}
-              moveRangeOnFirstSelection={false}
-              ranges={date}
-            /> */}
-          </div>
+          <Filter />
         </div>
       </div>
       <ModalTransaction

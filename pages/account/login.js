d9a79943b6b81d84b8a65be6bda980dbcc698cc7
harshpandey002 <coding.harshp@gmail.com>
2021-12-08 20:33:35 +0530

    Login signup Complete

--- pages/account/login.js
+++ pages/account/login.js
@@ -3,11 +3,39 @@ import styles from "@/styles/Account.module.css";
 import Link from "next/link";
 
 export default function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.blue} />
       <div className={styles.green} />
       <Navlinks />
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <h2>Login</h2>
+          <p>
+            Don{"'"}t have an account?{" "}
+            <Link href="/account/signup" passHref>
+              <span>Signup</span>
+            </Link>
+          </p>
+        </div>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.inputGroup}>
+            <p>Email</p>
+            <input type="text" placeholder="E-mail" />
+          </div>
+          <div className={styles.inputGroup}>
+            <p>Password</p>
+            <input type="text" placeholder="Password" />
+          </div>
+          <div className={styles.cta}>
+            <button className={styles.login}>Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- pages/account/signup.js
+++ pages/account/signup.js
@@ -3,6 +3,10 @@ import styles from "@/styles/Account.module.css";
 import Link from "next/link";
 
 export default function Signup() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.blue} />
@@ -12,29 +16,33 @@ export default function Signup() {
         <div className={styles.header}>
           <h2>Create account</h2>
           <p>
-            Already have and account?{" "}
+            Already have an account?{" "}
             <Link href="/account/login" passHref>
               <span>Login</span>
             </Link>
           </p>
         </div>
-        <form className={styles.form}>
+        <form onSubmit={handleSubmit} className={styles.form}>
           <div className={styles.flex}>
             <div className={styles.inputGroup}>
+              <p>First name</p>
               <input type="text" placeholder="First name" />
             </div>
             <div className={styles.inputGroup}>
+              <p>Last name</p>
               <input type="text" placeholder="Last name" />
             </div>
           </div>
           <div className={styles.inputGroup}>
+            <p>Email</p>
             <input type="text" placeholder="E-mail" />
           </div>
           <div className={styles.inputGroup}>
+            <p>Password</p>
             <input type="text" placeholder="Password" />
           </div>
           <div className={styles.cta}>
-            <button>Signup</button>
+            <button className={styles.signup}>Signup</button>
           </div>
         </form>
       </div>

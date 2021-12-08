502214efa6f2967ad627fdf370be45c760e3207c
harshpandey002 <coding.harshp@gmail.com>
2021-12-08 16:35:35 +0530

    Form Structure

--- pages/account/signup.js
+++ pages/account/signup.js
@@ -8,6 +8,36 @@ export default function Signup() {
       <div className={styles.blue} />
       <div className={styles.green} />
       <Navlinks />
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <h2>Create account</h2>
+          <p>
+            Already have and account?{" "}
+            <Link href="/account/login" passHref>
+              <span>Login</span>
+            </Link>
+          </p>
+        </div>
+        <form className={styles.form}>
+          <div className={styles.flex}>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="First name" />
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="text" placeholder="Last name" />
+            </div>
+          </div>
+          <div className={styles.inputGroup}>
+            <input type="text" placeholder="E-mail" />
+          </div>
+          <div className={styles.inputGroup}>
+            <input type="text" placeholder="Password" />
+          </div>
+          <div className={styles.cta}>
+            <button>Signup</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }

9f3a53af80a3e157782ccdde92c5a80290bdfd79
harshpandey002 <coding.harshp@gmail.com>
2021-12-10 00:47:42 +0530

    Signup Validation

--- pages/account/signup.js
+++ pages/account/signup.js
@@ -6,8 +6,46 @@ import Link from "next/link";
 export default function Signup() {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
+  const [formData, setFormData] = useState({
+    first: "",
+    last: "",
+    email: "",
+    password: "",
+  });
+
+  const validate = () => {
+    setError("");
+    setSuccess("");
+    try {
+      if (!formData.first) {
+        throw "Enter first name";
+      }
+      if (!formData.last) {
+        throw "Enter last name";
+      }
+      if (!formData.email) {
+        throw "Enter your email";
+      }
+      if (!formData.first) {
+        throw "Enter password";
+      }
+      return true;
+    } catch (err) {
+      setError(err);
+      return false;
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!validate()) return;
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
@@ -23,20 +61,44 @@ export default function Signup() {
           <div className={styles.flex}>
             <div className={styles.inputGroup}>
               {/* <p>First name</p> */}
-              <input type="text" placeholder="First name" />
+              <input
+                type="text"
+                placeholder="First name"
+                name="first"
+                value={formData.first}
+                onChange={handleChange}
+              />
             </div>
             <div className={styles.inputGroup}>
               {/* <p>Last name</p> */}
-              <input type="text" placeholder="Last name" />
+              <input
+                type="text"
+                placeholder="Last name"
+                name="last"
+                value={formData.last}
+                onChange={handleChange}
+              />
             </div>
           </div>
           <div className={styles.inputGroup}>
             {/* <p>Email</p> */}
-            <input type="email" placeholder="E-mail" />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
           </div>
           <div className={styles.inputGroup}>
             {/* <p>Password</p> */}
-            <input type="password" placeholder="Password" />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
           </div>
           <div className={styles.cta}>
             {error && <p className={styles.error}>{error}</p>}

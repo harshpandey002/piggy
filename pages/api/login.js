3b9f2e615b06f5c79e7ddc70194b4ccbbe0122c3
harshpandey002 <coding.harshp@gmail.com>
2021-12-11 22:10:34 +0530

    connected to db

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import initDB from "@/helpers/initDb";
+
+initDB();
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
